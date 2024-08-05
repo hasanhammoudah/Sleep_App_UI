bb00a4805486892eb3e4306932c6d17c356ac11e
hasanhammoudah <hassun.h98@gmail.com>
2024-08-05 22:02:35 +0300

    NEW UPDATE

---
+++ lib/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:sleep_app_ui/app_styles.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kBlackRich12,
+    );
+  }
+}
